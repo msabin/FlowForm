1ee35d9b9b4072f6a990f320000403602e569d84
Manuel Sabin <msabin27@gmail.com>
2024-04-25 09:19:38 -0700

    Form made more idiomatic.  About focus on stylings.

--- signup-flow/src/components/pages/PersonalInfo.jsx
+++ signup-flow/src/components/pages/PersonalInfo.jsx
@@ -11,24 +11,40 @@ export function PersonalInfo() {
         <div className='form-input'>
           <label htmlFor="name">Name</label>
           <input 
-            required type="text" 
+            type="text" 
             name="name" 
+            id="name"
             placeholder={"e.g. Stephen King"}
+            autoComplete='name'
+            required
          />
         </div>
 
         <div className='form-input'>
           <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" placeholder="e.g. stephenking@lorem.com"></input>
+          <input 
+            type="email" 
+            name="email" 
+            id="email"
+            placeholder="e.g. stephenking@lorem.com"
+            autoComplete='email'
+            required
+          />
         </div>
 
         <div className='form-input'>
           <label htmlFor="phone">Phone Number</label>
-          <input type="tel" name="phone" placeholder="e.g. +1 234 567 890"></input>
+          <input 
+            type="tel" 
+            name="phone"
+            id="phone"
+            placeholder="e.g. +1 234 567 890"
+            autoComplete='tel'
+            required
+          />
         </div>
 
-        <input type="submit" value="button" onClick={() => onSubmitForm}/>
-
+        
       </form>
 
     </div>
