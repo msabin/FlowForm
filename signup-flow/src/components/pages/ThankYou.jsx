af3336e3e096b5db8deca185a09fea9f2bd5dbda
Manuel Sabin <msabin27@gmail.com>
2024-05-19 00:24:31 -0700

    Thank you page is done and responsive.  The project looks just about complete.  Just need to add some error-checking on the text form for Personal Info page.

--- signup-flow/src/components/pages/ThankYou.jsx
+++ signup-flow/src/components/pages/ThankYou.jsx
@@ -1,11 +1,18 @@
 import '../../styles/components/pages/thankyou.scss'
+import thankYouSVG from '../../assets/images/icon-thank-you.svg'
 
 export function ThankYou() {
   return (
-    <div className="page-container">
-      <div className="content-container">
-        Thank you
-      </div>
-    </div>
+    <div className="page-container" id="thank-you-container">
+      <img src={thankYouSVG}/>
+
+      <h1>Thank you!</h1>
+
+      <p className='page-desc'>
+        Thanks for confirming your subscription!  We hope you have fun using our
+        platform.  If you ever need support, please feel free to email us at
+        support@loremgaming.com.
+      </p>
+</div>
   )
 }
